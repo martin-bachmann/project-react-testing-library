8ae9f4908467afe5f484445e3dd7aa27e978d486
Martin Bachmann <martinpezbach@gmail.com>
2022-06-19 03:18:05 -0300

    Requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('06 - Teste o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de um pokemon', () => {
+    renderWithRouter(<App />);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeType = screen.getByTestId('pokemon-type');
+    const pokeWeight = screen.getByTestId('pokemon-weight');
+    const pokeImage = screen.getByAltText(/sprite/);
+
+    expect(pokeName).toHaveTextContent(/Pikachu/);
+    expect(pokeType).toHaveTextContent(/eletric/);
+    expect(pokeWeight).toHaveTextContent(/Average weight: 6.0 kg/);
+    expect(pokeImage).toBeInTheDocument();
+    expect(pokeImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('06 - Teste o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de um pokemon', () => {
+    renderWithRouter(<App />);
+
+    const pokeName = screen.getByTestId('pokemon-name');
+    const pokeType = screen.getByTestId('pokemon-type');
+    const pokeWeight = screen.getByTestId('pokemon-weight');
+    const pokeImage = screen.getByAltText(/sprite/);
+
+    expect(pokeName).toHaveTextContent(/Pikachu/);
+    expect(pokeType).toHaveTextContent(/Electric/);
+    expect(pokeWeight).toHaveTextContent(/Average weight: 6.0 kg/);
+    expect(pokeImage).toBeInTheDocument();
+    expect(pokeImage).toHaveAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png');
+  });
+  it('Teste se contém um link de navegação para exibir detalhes', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /More details/ });
+
+    expect(moreDetails).toBeInTheDocument();
+    expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+  });
+  it('Teste se ao clicar no link de de detalhes abre página de detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /More details/ });
+    userEvent.click(moreDetails);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  it('Teste se pokemon favoritado recebe destaque', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /More details/ });
+    userEvent.click(moreDetails);
+
+    const favorite = screen.getByLabelText(/Pokémon favoritado?/);
+    userEvent.click(favorite);
+
+    const pokeImage = screen.getByAltText(/is marked as favorite/);
+
+    expect(pokeImage).toBeInTheDocument();
+    expect(pokeImage).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
