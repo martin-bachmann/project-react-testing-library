3d02cfd0208443002f0f4ea1457425692bdbbef8
Martin Bachmann <martinpezbach@gmail.com>
2022-06-19 02:55:15 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('04 - Teste o componente <NotFound.js />', () => {
+  it('Teste se a pagina contém um título', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: /Page requested not found/ });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+  it('Teste se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const pokedexImage = screen.getByAltText(/Pikachu crying because the page requested/);
+
+    expect(pokedexImage).toBeInTheDocument();
+    expect(pokedexImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
