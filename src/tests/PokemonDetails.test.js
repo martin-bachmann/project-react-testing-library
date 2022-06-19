59ddd71528a3307bcae8618120987264831fdc03
Martin Bachmann <martinpezbach@gmail.com>
2022-06-19 15:32:28 -0300

    Requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -28,7 +28,44 @@ describe('07 - Teste o componente <PokemonDetails.js />', () => {
     userEvent.click(moreDetails);
 
     const mapsTitle = screen.getByRole('heading', { name: /Game Locations of/ });
+    const locationArray = screen.getAllByAltText(/location/);
 
     expect(mapsTitle).toBeInTheDocument();
+    expect(pokemons[0].foundAt).toHaveLength(locationArray.length);
+
+    locationArray.forEach((location, index) => {
+      expect(location).toHaveAttribute('src', pokemons[0].foundAt[index].map);
+    });
+  });
+  it('Teste se pode favoritar um pokemon', () => {
+    renderWithRouter(<App />);
+
+    // Verifica a adição de um pokemon
+    const moreDetails = screen.getByRole('link', { name: /More details/ });
+    userEvent.click(moreDetails);
+
+    const favorite = screen.getByLabelText(/Pokémon favoritado?/);
+    expect(favorite).toBeInTheDocument();
+    userEvent.click(favorite);
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+
+    const favPokemons = screen.getAllByTestId('pokemon-name');
+    expect(favPokemons).toHaveLength(1);
+
+    // Verifica a remoção de um pokemon
+    const moreDetails2 = screen.getByRole('link', { name: /More details/ });
+    userEvent.click(moreDetails2);
+
+    const favorite2 = screen.getByLabelText(/Pokémon favoritado?/);
+    expect(favorite2).toBeInTheDocument();
+    userEvent.click(favorite2);
+
+    const favoriteLink2 = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink2);
+
+    const favPokemons2 = screen.queryAllByTestId('pokemon-name');
+    expect(favPokemons2).toHaveLength(0);
   });
 });
